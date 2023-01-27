87cb6c49a8bd3c6d299e115d86ec0e8c31f66dfc
Oleh Okarynskyi <okarynskyy@gmail.com>
2023-01-27 13:31:21 +0200

    init project

---
+++ server.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const app = require('./app')
+
+const { DB_HOST } = process.env;
+
+
+mongoose.connect(DB_HOST)
+  .then(() => app.listen(3000, () => {
+  console.log("Database connection successful")
+}))
+  .catch(err => {
+    console.log(err);
+    process.exit(1);
+  })
