6cc46ca3fa9c0f9908f2ef32a031da0c930ac557
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2023-01-29 17:58:01 +0200

    prepare work space

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
-const NoticePet = require("./noticePets");
+const Notice = require("./notices");
 const User = require("./users");
 const UserPet = require("./userPets");
 const News = require("./news");
 const Friend = require("./friends");
 
 module.exports = {
-  NoticePet,
+  Notice,
   User,
   UserPet,
   News,
---
+++ models/notices.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require("mongoose");
+
+const noticeSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    birthday: {
+        type: Date,
+        required: true, 
+    },
+    breed: {
+        type: String,
+        required: true, 
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    sex: {
+        type: String,
+        enum: ["male", "female"],
+        required: true,
+    },
+    comments: {
+        type: String,
+        // обов`язкове поле?
+    },
+    avatarURL: {
+        type: String
+    },
+    adopStatus: {
+        type: String,
+        enum: ["lost/found", "sell", "in good hands"],
+        required: true,
+    },
+    price: {
+        type: Number,        
+    },
+    owner: { // userId
+        type: Schema.Types.ObjectId,
+        ref: "user",
+    },
+    ownerEmail: { // додається на бєку по запиту на юзера
+        type: String,
+        required: true,
+    },
+    ownerPhone: { // додається на бєку по запиту на юзера
+        type: String,
+        required: true,
+    },
+})
+
+const Notice = model("notice", noticeSchema);
+
+module.exports = Notice;
