590f9f3b22532bd7ea1abd7647aa627a3b762096
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2023-01-30 10:25:13 +0200

    add check if ID is in array

--- controllers/notices/addToFavorite.js
+++ controllers/notices/addToFavorite.js
@@ -1,3 +1,4 @@
+const { HttpError } = require('../../helpers');
 const { User } = require('../../models');
 
 const addToFavorite = async (req, res) => {
@@ -5,8 +6,13 @@ const addToFavorite = async (req, res) => {
     const { _id } = req.user;
     const { noticeId } = req.params;
 
-    const {favorites} = await User.findById({ _id });
-    
+    const { favorites } = await User.findById({ _id });
+   
+    const index = favorites.indexOf(noticeId);
+    if (index !== -1) {
+        throw HttpError(409, 'ID in use')
+    }
+        
     favorites.push(noticeId);
 
     const user = await User.findByIdAndUpdate({ _id }, { favorites }, { new: true });
--- controllers/notices/removeFromFavorite.js
+++ controllers/notices/removeFromFavorite.js
@@ -1,3 +1,4 @@
+const { HttpError } = require('../../helpers');
 const { User } = require('../../models');
 
 const removeFromFavorite = async (req, res) => {
@@ -8,9 +9,10 @@ const removeFromFavorite = async (req, res) => {
     const {favorites} = await User.findById({ _id });
     
     const index = favorites.indexOf(noticeId);
-
     if (index > -1) {
        favorites.splice(index, 1)
+    } else {
+        throw HttpError(400, 'ID not in favorites')
     }
 
     const user = await User.findByIdAndUpdate({ _id }, { favorites }, { new: true });
