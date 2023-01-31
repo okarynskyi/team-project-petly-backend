7eb7a2e03a0d4e68b111d637728d3d88c42be374
Yurii Kukharuk <yurakukharuk@gmail.com>
2023-01-31 18:29:22 +0200

    Test

--- services/cloudinary/uploadImgToCloudinary.js
+++ services/cloudinary/uploadImgToCloudinary.js
@@ -1,21 +1,25 @@
-const cloudinary = require("./cloudinary");
+const cloudinary = require('./cloudinary');
 
 const uploadImgToCloudinary = async (req, w, h) => {
   let imageURL = null;
 
-  if (req.file) {
-    const path = req.file.path;
+    if (req.file) {
+        const path = req.file.path
 
-    const uploader = async (path) => await cloudinary.uploads(path, "Images");
+        const uploader = async (path) => await cloudinary.uploads(path, 'Images')
 
-    const newURL = await uploader(path);
+        const newURL = await uploader(path)
 
-    const id = newURL.id;
+        let id = newURL.id
 
-    imageURL = await cloudinary.createImageTag(id, w, h);
-  }
+       imageURL = await cloudinary.createImageTag(id, w, h)
+    }
+    
 
-  return imageURL;
-};
+    
+    
+    
+    return imageURL
+}
 
 module.exports = uploadImgToCloudinary;
