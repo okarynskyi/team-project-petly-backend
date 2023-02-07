2f92160547bab661fd4aabdba41a2956c510862d
Yana Pavlyk <pavlikyg@gmail.com>
2023-02-07 12:48:29 +0200

    Update create.js

--- controllers/notices/create.js
+++ controllers/notices/create.js
@@ -1,19 +1,19 @@
 const { Notice } = require("../../models");
-const { HttpError } = require("../../helpers");
+// const { HttpError } = require("../../helpers");
 const { uploadImgToCloudinary } = require("../../services/cloudinary");
 
 const create = async (req, res) => {
   const { _id } = req.user;
-  const { adopStatus, title } = req.body;
+  // const { adopStatus, title } = req.body;
 
-  const notice = await Notice.findOne({ adopStatus, title, owner: _id });
+  // const notice = await Notice.findOne({ adopStatus, title, owner: _id });
 
-  if (notice) {
-    throw HttpError(
-      409,
-      `Notise "${title}" already exist in "${adopStatus}" category`
-    );
-  }
+  // if (notice) {
+  //   throw HttpError(
+  //     409,
+  //     `Notise "${title}" already exist in "${adopStatus}" category`
+  //   );
+  // }
 
   const avatarURL = await uploadImgToCloudinary(req, 336, 336);
 
