17f02732ff9f5b46e9b95d74699e216da2b6a083
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2023-02-07 10:58:20 +0200

    fixed

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,7 +1,7 @@
 const Joi = require("joi")
 
 const signupSchema = Joi.object({
-    email: Joi.string().trim(true).min(7).max(63).email().required(),
+    email: Joi.string().min(7).max(63).email().required(),
     password: Joi.string().trim(true).min(7).max(32).pattern(/^\S*$/).required(),
     name: Joi.string().alphanum().required(),
     location: Joi.string().regex(/[A-Z][a-z]*,\s[A-Z][a-z]*/).required(),
@@ -10,13 +10,13 @@ const signupSchema = Joi.object({
 })
 
 const loginSchema = Joi.object({
-    email: Joi.string().trim(true).min(7).max(63).email().required(),
+    email: Joi.string().min(7).max(63).email().required(),
     password: Joi.string().trim(true).min(7).max(32).pattern(/^\S*$/).required()
 })
 
 const userUpdateSchema = Joi.object({
     name: Joi.string().alphanum(),
-    email: Joi.string().trim(true).min(7).max(63).email(),
+    email: Joi.string().min(7).max(63).email(),
     birthday: Joi.date(),
     phone: Joi.string().length(13).pattern(/^\+[380]{3}\d{7}/),
     location: Joi.string().regex(/[A-Z][a-z]*,\s[A-Z][a-z]*/)
